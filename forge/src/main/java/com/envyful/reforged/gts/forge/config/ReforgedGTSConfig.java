3d146f6e6f5c4354ecb0e279a1e35b6c4f3412d2
danorris709 <dnorris443@gmail.com>
2021-08-25 13:18:41 +0100

    Feat: started config

---
+++ forge/src/main/java/com/envyful/reforged/gts/forge/config/ReforgedGTSConfig.java
@@ -0,0 +1,54 @@
+package com.envyful.reforged.gts.forge.config;
+
+import com.envyful.api.config.data.ConfigPath;
+import com.envyful.api.config.yaml.AbstractYamlConfig;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+@ConfigPath("config/ReforgedGTS/config.yml")
+public class ReforgedGTSConfig extends AbstractYamlConfig {
+
+    private DatabaseDetails databaseDetails = new DatabaseDetails();
+
+    public ReforgedGTSConfig() {
+        super();
+    }
+
+    public DatabaseDetails getDatabaseDetails() {
+        return this.databaseDetails;
+    }
+
+    @ConfigSerializable
+    public static class DatabaseDetails {
+
+        private String poolName = "ReforgedGTS";
+        private String ip = "0.0.0.0";
+        private String port = "3306";
+        private String username = "admin";
+        private String password = "admin";
+        private String database = "database";
+
+        public String getPoolName() {
+            return this.poolName;
+        }
+
+        public String getIp() {
+            return this.ip;
+        }
+
+        public String getPort() {
+            return this.port;
+        }
+
+        public String getUsername() {
+            return this.username;
+        }
+
+        public String getPassword() {
+            return this.password;
+        }
+
+        public String getDatabase() {
+            return this.database;
+        }
+    }
+}
