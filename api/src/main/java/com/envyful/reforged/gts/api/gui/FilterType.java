52b4bf69b8c1dc192f856fb863d5ea2bc19eff37
danorris709 <dnorris443@gmail.com>
2021-08-25 13:03:08 +0100

    Feat: added predicate filter

--- api/src/main/java/com/envyful/reforged/gts/api/gui/FilterType.java
+++ api/src/main/java/com/envyful/reforged/gts/api/gui/FilterType.java
@@ -1,5 +1,8 @@
 package com.envyful.reforged.gts.api.gui;
 
+import java.util.Objects;
+import java.util.function.Predicate;
+
 /**
  *
  * Enum used for filtering for specific {@link com.envyful.reforged.gts.api.Trade} types in the GUI
@@ -7,19 +10,25 @@ package com.envyful.reforged.gts.api.gui;
  */
 public enum FilterType {
 
-    INSTANT_BUY("Instant Buy"),
-    AUCTION("Auction"),
-    ALL("All")
+    INSTANT_BUY("Instant Buy", type -> type.getDisplayName().equalsIgnoreCase("Instant Buy")),
+    AUCTION("Auction", type -> type.getDisplayName().equalsIgnoreCase("Auction")),
+    ALL("All", type -> true)
 
     ;
 
-    private String displayName;
+    private final String displayName;
+    private final Predicate<FilterType> predicate;
 
-    FilterType(String displayName) {
+    FilterType(String displayName, Predicate<FilterType> predicate) {
         this.displayName = displayName;
+        this.predicate = predicate;
     }
 
     public String getDisplayName() {
         return this.displayName;
+    }
+
+    public boolean isAllowed(FilterType type) {
+        return this.predicate.test(type);
     }
 }
