d2ded78997777bd80ea042cf9db26c5fd04c126b
danorris709 <dnorris443@gmail.com>
2021-08-25 13:59:45 +0100

    Feat: implemented methods in ForgeGlobalTradeManager.java

--- forge/src/main/java/com/envyful/reforged/gts/forge/impl/ForgeGlobalTradeManager.java
+++ forge/src/main/java/com/envyful/reforged/gts/forge/impl/ForgeGlobalTradeManager.java
@@ -3,35 +3,70 @@ package com.envyful.reforged.gts.forge.impl;
 import com.envyful.api.player.EnvyPlayer;
 import com.envyful.reforged.gts.api.GlobalTradeManager;
 import com.envyful.reforged.gts.api.Trade;
+import com.envyful.reforged.gts.forge.ReforgedGTSForge;
+import com.envyful.reforged.gts.forge.player.GTSAttribute;
+import com.google.common.collect.Lists;
 
+import java.util.Collections;
 import java.util.List;
 
-public class ForgeGlobalTradeManager implements GlobalTradeManager {
+public abstract class ForgeGlobalTradeManager implements GlobalTradeManager {
 
-    //TODO: implement methods
+    private final List<Trade> activeTrades = Lists.newArrayList();
+
+    public ForgeGlobalTradeManager() {}
 
     @Override
     public void addTrade(EnvyPlayer<?> player, Trade trade) {
+        GTSAttribute attribute = player.getAttribute(ReforgedGTSForge.class);
 
+        if (attribute == null) {
+            return;
+        }
+
+        this.activeTrades.add(trade);
+        attribute.getOwnedTrades().add(trade);
     }
 
     @Override
     public List<Trade> getAllTrades() {
-        return null;
+        return this.activeTrades;
     }
 
     @Override
     public List<Trade> getUserTrades(EnvyPlayer<?> player) {
-        return null;
+        List<Trade> trades = Lists.newArrayList();
+
+        for (Trade activeTrade : this.activeTrades) {
+            if (activeTrade.isOwner(player)) {
+                trades.add(activeTrade);
+            }
+        }
+
+        return trades;
     }
 
     @Override
     public List<Trade> getExpiredTrades(EnvyPlayer<?> player) {
-        return null;
+        GTSAttribute attribute = player.getAttribute(ReforgedGTSForge.class);
+
+        if (attribute == null) {
+            return Collections.emptyList();
+        }
+
+        List<Trade> expired = Lists.newArrayList();
+
+        for (Trade ownedTrade : attribute.getOwnedTrades()) {
+            if (ownedTrade.hasExpired()) {
+                expired.add(ownedTrade);
+            }
+        }
+
+        return expired;
     }
 
     @Override
     public List<Trade> getPurchasedTrades(EnvyPlayer<?> player) {
-        return null;
+        return null; //TODO:
     }
 }
