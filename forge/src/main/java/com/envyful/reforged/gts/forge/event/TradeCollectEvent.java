3afd2372dc4290a749cc09ee9de18c5164d6c33b
danorris709 <dnorris443@gmail.com>
2021-08-26 19:56:21 +0100

    Feat: added TradeCollectEvent

---
+++ forge/src/main/java/com/envyful/reforged/gts/forge/event/TradeCollectEvent.java
@@ -0,0 +1,32 @@
+package com.envyful.reforged.gts.forge.event;
+
+import com.envyful.api.player.EnvyPlayer;
+import com.envyful.reforged.gts.api.Trade;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+/**
+ *
+ * Represents when a player collects a purchased or expired trade from the GTS
+ *
+ */
+@Cancelable
+public class TradeCollectEvent extends Event {
+
+    private final EnvyPlayer<EntityPlayerMP> collector;
+    private final Trade trade;
+
+    public TradeCollectEvent(EnvyPlayer<EntityPlayerMP> collector, Trade trade) {
+        this.collector = collector;
+        this.trade = trade;
+    }
+
+    public EnvyPlayer<EntityPlayerMP> getCollector() {
+        return this.collector;
+    }
+
+    public Trade getTrade() {
+        return this.trade;
+    }
+}
