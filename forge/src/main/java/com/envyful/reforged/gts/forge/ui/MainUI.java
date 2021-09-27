63b9af5e8ba49ebcab070735971aa9a7dfcc38a6
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-09-27 11:03:02 +0100

    feat(forge): implement gui settings

--- forge/src/main/java/com/envyful/reforged/gts/forge/ui/MainUI.java
+++ forge/src/main/java/com/envyful/reforged/gts/forge/ui/MainUI.java
@@ -1,8 +1,8 @@
 package com.envyful.reforged.gts.forge.ui;
 
+import com.envyful.api.config.type.ConfigItem;
 import com.envyful.api.forge.items.ItemBuilder;
 import com.envyful.api.gui.factory.GuiFactory;
-import com.envyful.api.gui.item.Displayable;
 import com.envyful.api.gui.pane.Pane;
 import com.envyful.api.player.EnvyPlayer;
 import com.envyful.reforged.gts.forge.ReforgedGTSForge;
@@ -12,26 +12,27 @@ import net.minecraft.item.ItemStack;
 
 public class MainUI {
 
-    public static final Displayable BACKGROUND_ITEM = GuiFactory.displayableBuilder(ItemStack.class)
-            .itemStack(new ItemBuilder()
-                    .type(Item.getByNameOrId("minecraft:stained_glass_pane"))
-                    .damage(15)
-                    .build())
-            .build();
-
     public static void open(EnvyPlayer<EntityPlayerMP> player) {
         Pane pane = GuiFactory.paneBuilder()
                 .topLeftX(0).topLeftY(0)
                 .width(9)
-                .height(6)
+                .height(ReforgedGTSForge.getInstance().getConfig().getGuiSettings().getHeight())
                 .build();
 
-        pane.fill(BACKGROUND_ITEM);
+        for (ConfigItem fillerItem : ReforgedGTSForge.getInstance().getConfig().getGuiSettings().getFillerItems()) {
+            pane.add(GuiFactory.displayableBuilder(ItemStack.class).itemStack(new ItemBuilder()
+                             .type(Item.getByNameOrId(fillerItem.getType()))
+                             .amount(fillerItem.getAmount())
+                             .damage(fillerItem.getDamage())
+                             .name(fillerItem.getName())
+                             .lore(fillerItem.getLore())
+                             .build()).build());
+        }
 
         GuiFactory.guiBuilder()
                 .setPlayerManager(ReforgedGTSForge.getInstance().getPlayerManager())
                 .addPane(pane)
-                .height(5)
+                .height(ReforgedGTSForge.getInstance().getConfig().getGuiSettings().getHeight())
                 .title(ReforgedGTSForge.getInstance().getLocale().getGuiName())
                 .build().open(player);
     }
