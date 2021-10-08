7ab006891c19f842058d5c8cfacae3649e3f4798
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-08 15:09:42 +0100

    feat(forge): add settings saving and loading

--- forge/src/main/java/com/envyful/reforged/gts/forge/player/GTSAttribute.java
+++ forge/src/main/java/com/envyful/reforged/gts/forge/player/GTSAttribute.java
@@ -2,8 +2,10 @@ package com.envyful.reforged.gts.forge.player;
 
 import com.envyful.api.forge.player.ForgeEnvyPlayer;
 import com.envyful.api.forge.player.attribute.AbstractForgeAttribute;
+import com.envyful.api.json.UtilGson;
 import com.envyful.api.player.EnvyPlayer;
 import com.envyful.reforged.gts.api.Trade;
+import com.envyful.reforged.gts.api.player.PlayerSettings;
 import com.envyful.reforged.gts.api.sql.ReforgedGTSQueries;
 import com.envyful.reforged.gts.forge.ReforgedGTSForge;
 import com.envyful.reforged.gts.forge.impl.TradeFactory;
@@ -22,6 +24,7 @@ public class GTSAttribute extends AbstractForgeAttribute<ReforgedGTSForge> {
     private double currentPrice = -1;
     private double currentMinPrice = -1;
     private long currentDuration = -1;
+    private PlayerSettings settings = new PlayerSettings();
 
     public GTSAttribute(ReforgedGTSForge manager, EnvyPlayer<?> parent) {
         super(manager, (ForgeEnvyPlayer) parent);
@@ -63,10 +66,16 @@ public class GTSAttribute extends AbstractForgeAttribute<ReforgedGTSForge> {
         this.currentMinPrice = currentMinPrice;
     }
 
+    public PlayerSettings getSettings() {
+        return this.settings;
+    }
+
     @Override
     public void load() {
         try (Connection connection = this.manager.getDatabase().getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(ReforgedGTSQueries.GET_ALL_PLAYER)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(ReforgedGTSQueries.GET_ALL_PLAYER);
+             PreparedStatement settingsStatement = connection.prepareStatement(ReforgedGTSQueries.GET_PLAYER_SETTINGS)) {
+            preparedStatement.setString(1, this.parent.getUuid().toString());
             preparedStatement.setString(1, this.parent.getUuid().toString());
 
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -74,6 +83,14 @@ public class GTSAttribute extends AbstractForgeAttribute<ReforgedGTSForge> {
             while (resultSet.next()) {
                 this.ownedTrades.add(TradeFactory.fromResultSet(resultSet));
             }
+
+            ResultSet settingsSet = settingsStatement.executeQuery();
+
+            if (!settingsSet.next()) {
+                return;
+            }
+
+            this.settings = UtilGson.GSON.fromJson(resultSet.getString("settings"), PlayerSettings.class);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -82,11 +99,15 @@ public class GTSAttribute extends AbstractForgeAttribute<ReforgedGTSForge> {
     @Override
     public void save() {
         try (Connection connection = this.manager.getDatabase().getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(ReforgedGTSQueries.UPDATE_PLAYER_NAME)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(ReforgedGTSQueries.UPDATE_PLAYER_NAME);
+             PreparedStatement settingsStatement = connection.prepareStatement(ReforgedGTSQueries.UPDATE_OR_CREATE_SETTINGS)) {
             preparedStatement.setString(1, this.parent.getParent().getName());
             preparedStatement.setString(2, this.parent.getUuid().toString());
+            settingsStatement.setString(1, this.parent.getUuid().toString());
+            settingsStatement.setString(2, UtilGson.GSON.toJson(this.settings));
 
             preparedStatement.executeUpdate();
+            settingsStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
