c407f06125e2152ad3f4a9d0c3df85445a87222b
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-10 11:30:51 +0100

    feat(forge): add TradeRemoveEvent

---
+++ forge/src/main/java/com/envyful/reforged/gts/forge/event/TradeRemoveEvent.java
@@ -0,0 +1,24 @@
+package com.envyful.reforged.gts.forge.event;
+
+import com.envyful.reforged.gts.api.Trade;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+/**
+ *
+ * Represents when a Trade is removed from the GTS
+ *
+ */
+@Cancelable
+public class TradeRemoveEvent extends Event {
+
+    private final Trade trade;
+
+    public TradeRemoveEvent(Trade trade) {
+        this.trade = trade;
+    }
+
+    public Trade getTrade() {
+        return this.trade;
+    }
+}
