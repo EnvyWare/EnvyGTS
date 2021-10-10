e4557054de8ff122db836c1bf71f6258716e9eeb
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-10 17:05:23 +0100

    chore(gradle): version bump

--- forge/src/main/java/com/envyful/reforged/gts/forge/ReforgedGTSForge.java
+++ forge/src/main/java/com/envyful/reforged/gts/forge/ReforgedGTSForge.java
@@ -39,7 +39,7 @@ import java.sql.SQLException;
 )
 public class ReforgedGTSForge {
 
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.4.0";
 
     private static ReforgedGTSForge instance;
 
