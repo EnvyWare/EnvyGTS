cbc7adbd88dbe56bd85f0d9ff80f3320a15a5112
Daniel Norris <dnorris443@gmail.com>
2021-11-09 20:32:18 +0000

    feat(forge): add config toggle for eggs

--- forge/src/main/java/com/envyful/reforged/gts/forge/ui/SelectPartyPokemonUI.java
+++ forge/src/main/java/com/envyful/reforged/gts/forge/ui/SelectPartyPokemonUI.java
@@ -123,7 +123,7 @@ public class SelectPartyPokemonUI {
             if (i >= all.length || all[i] == null) {
                 pane.set(pos % 9, pos / 9, GuiFactory.displayableBuilder(ItemStack.class)
                         .itemStack(UtilConfigItem.fromConfigItem(config.getNoPokemonItem())).build());
-            } else if (all[i].hasSpecFlag("untradeable") || all[i].isEgg() || ReforgedGTSForge.getInstance().getConfig().isBlackListed(all[i])) {
+            } else if (all[i].hasSpecFlag("untradeable") || (!ReforgedGTSForge.getInstance().getConfig().isAllowEggs() && all[i].isEgg()) || ReforgedGTSForge.getInstance().getConfig().isBlackListed(all[i])) {
                 pane.set(pos % 9, pos / 9, GuiFactory.displayableBuilder(ItemStack.class)
                         .itemStack(UtilConfigItem.fromConfigItem(config.getUntradeablePokemonItem())).build());
             } else {
