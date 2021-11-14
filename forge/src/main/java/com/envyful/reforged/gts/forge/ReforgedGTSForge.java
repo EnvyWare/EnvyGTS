bbdc708e03012f3e9065a8fd1d8b0e0dfa07357f
Daniel Norris <dnorris443@gmail.com>
2021-11-14 17:34:26 +0000

    chore(gradle): bump version

--- forge/src/main/java/com/envyful/reforged/gts/forge/ReforgedGTSForge.java
+++ forge/src/main/java/com/envyful/reforged/gts/forge/ReforgedGTSForge.java
@@ -39,7 +39,7 @@ import java.sql.SQLException;
 )
 public class ReforgedGTSForge {
 
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.9.0";
 
     private static ReforgedGTSForge instance;
 
