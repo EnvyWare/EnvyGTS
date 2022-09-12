f0d9326dd0a64497a3a99acbbb2acb7672df2d45
Daniel Norris <dnorris443@gmail.com>
2022-09-12 15:49:23 +0100

    chore(gradle): bump version to 3.0.1

--- forge16/src/main/java/com/envyful/gts/forge/EnvyGTSForge.java
+++ forge16/src/main/java/com/envyful/gts/forge/EnvyGTSForge.java
@@ -37,7 +37,7 @@ import java.sql.SQLException;
 @Mod("envygts")
 public class EnvyGTSForge {
 
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "3.0.1";
 
     private static EnvyGTSForge instance;
 
