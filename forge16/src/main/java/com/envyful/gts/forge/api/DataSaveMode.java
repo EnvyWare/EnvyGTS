71ee1dd365ab2fbc6beca0c2d9014405a92f57b4
M4RC0Sx <marcos3as3@gmail.com>
2023-05-07 18:23:06 +0200

    feat: sqlite handling in config and hikari

--- forge16/src/main/java/com/envyful/gts/forge/api/DataSaveMode.java
+++ forge16/src/main/java/com/envyful/gts/forge/api/DataSaveMode.java
@@ -1,2 +1,7 @@
-package com.envyful.gts.forge.api;public enum DataSaveMode {
+package com.envyful.gts.forge.api;
+
+public enum DataSaveMode {
+
+    SQLITE(),
+    MYSQL();
 }
--- forge16/src/main/java/com/envyful/gts/forge/api/SimpleHikariSQLiteDatabase.java
+++ forge16/src/main/java/com/envyful/gts/forge/api/SimpleHikariSQLiteDatabase.java
@@ -1,7 +1,7 @@
-package com.envyful.gts.forge.util;
+package com.envyful.gts.forge.api;
 
 import com.envyful.api.config.type.SQLDatabaseDetails;
-import com.envyful.api.database.impl.SimpleHikariDatabase;
+import com.envyful.api.database.Database;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -9,43 +9,18 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
-public class SimpleHikariSQLiteDatabase extends SimpleHikariDatabase {
-    public SimpleHikariSQLiteDatabase(SQLDatabaseDetails details) {
-        super(details);
-    }
-}
-
+public class SimpleHikariSQLiteDatabase implements Database {
 
     private HikariDataSource hikari;
 
-    public SimpleHikariDatabase(SQLDatabaseDetails details) {
-        this(details.getConnectionUrl(), details.getPoolName(), details.getIp(), details.getPort(),
-                details.getUsername(), details.getPassword(), details.getDatabase(), details.getMaxPoolSize(), details.getMaxLifeTimeSeconds()
-        );
-    }
-
-    public SimpleHikariDatabase(String name, String ip, int port, String username, String password, String database) {
-        this(null, name, ip, port, username, password, database, 30, 30);
-    }
-
-    public SimpleHikariDatabase(String connectionUrl, String name, String ip, int port, String username,
-                                String password, String database, int maxConnections, long maxLifeTime) {
+    public SimpleHikariSQLiteDatabase(SQLDatabaseDetails details) {
         HikariConfig config = new HikariConfig();
 
-        config.setMaximumPoolSize(Math.max(1, maxConnections));
-        config.setPoolName(name);
+        config.setMaximumPoolSize(Math.max(1, details.getMaxPoolSize()));
+        config.setPoolName(details.getPoolName());
 
-        if (connectionUrl == null) {
-            config.setJdbcUrl("jdbc:mysql://" + ip + ":" + port + "/" + database);
-        } else {
-            config.setJdbcUrl(connectionUrl);
-        }
+        config.setJdbcUrl("jdbc:sqlite:config/EnvyGTS/gts.sqlite");
 
-        config.addDataSourceProperty("serverName", ip);
-        config.addDataSourceProperty("port", port);
-        config.addDataSourceProperty("databaseName", database);
-        config.addDataSourceProperty("user", username);
-        config.addDataSourceProperty("password", password);
         config.addDataSourceProperty("cachePrepStmts", true);
         config.addDataSourceProperty("prepStmtCacheSize", 250);
         config.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
@@ -57,8 +32,8 @@ public class SimpleHikariSQLiteDatabase extends SimpleHikariDatabase {
         config.addDataSourceProperty("useLocalSessionState", true);
         config.addDataSourceProperty("characterEncoding","utf8");
         config.addDataSourceProperty("useUnicode","true");
-        config.addDataSourceProperty("maxLifetime", TimeUnit.SECONDS.toMillis(maxLifeTime));
-        config.setMaxLifetime(TimeUnit.SECONDS.toMillis(maxLifeTime));
+        config.addDataSourceProperty("maxLifetime", TimeUnit.SECONDS.toMillis(details.getMaxLifeTimeSeconds()));
+        config.setMaxLifetime(TimeUnit.SECONDS.toMillis(details.getMaxLifeTimeSeconds()));
         config.setConnectionTimeout(TimeUnit.SECONDS.toMillis(30));
         config.setLeakDetectionThreshold(TimeUnit.SECONDS.toMillis(60));
         config.setConnectionTestQuery("/* Ping */ SELECT 1");
@@ -75,3 +50,4 @@ public class SimpleHikariSQLiteDatabase extends SimpleHikariDatabase {
     public void close() {
         this.hikari.close();
     }
+}
