75783f0ae7256bf1f5da7500d73f55f174d45392
Daniel Norris <dnorris443@gmail.com>
2023-05-16 22:43:37 +0100

    feat(api): add GTS reload event

--- forge16/src/main/java/com/envyful/gts/forge/command/ReloadCommand.java
+++ forge16/src/main/java/com/envyful/gts/forge/command/ReloadCommand.java
@@ -6,9 +6,11 @@ import com.envyful.api.command.annotate.Permissible;
 import com.envyful.api.command.annotate.executor.CommandProcessor;
 import com.envyful.api.command.annotate.executor.Sender;
 import com.envyful.gts.forge.EnvyGTSForge;
+import com.envyful.gts.forge.event.GTSReloadEvent;
 import net.minecraft.command.ICommandSource;
 import net.minecraft.util.Util;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.common.MinecraftForge;
 
 @Command(
         value = "reload",
@@ -21,6 +23,7 @@ public class ReloadCommand {
     @CommandProcessor
     public void onCommand(@Sender ICommandSource sender, String[] args) {
         EnvyGTSForge.getInstance().loadConfig();
+        MinecraftForge.EVENT_BUS.post(new GTSReloadEvent());
         sender.sendMessage(new StringTextComponent("Reloaded config"), Util.NIL_UUID);
     }
 }
