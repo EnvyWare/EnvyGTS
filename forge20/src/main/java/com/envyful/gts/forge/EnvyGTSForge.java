66925447c74e2e5773dd30a209c44a78d2382be2
Daniel Norris <dnorris443@gmail.com>
2024-02-04 08:36:59 +0000

    chore(gradle): bump to 4.5.5

--- forge20/src/main/java/com/envyful/gts/forge/EnvyGTSForge.java
+++ forge20/src/main/java/com/envyful/gts/forge/EnvyGTSForge.java
@@ -43,7 +43,7 @@ import java.io.IOException;
 @Mod("envygts")
 public class EnvyGTSForge {
 
-    public static final String VERSION = "4.5.4";
+    public static final String VERSION = "4.5.5";
 
     private static Logger LOGGER = LogManager.getLogger("EnvyGTS");
 
