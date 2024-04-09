1a11ac57e8e2bb6341d12366eaf3473b627d3b93
Daniel Norris <dnorris443@gmail.com>
2024-04-09 23:14:14 +0100

    fix(forge20): update dialogue usage

--- forge20/src/main/java/com/envyful/gts/forge/ui/EditDurationUI.java
+++ forge20/src/main/java/com/envyful/gts/forge/ui/EditDurationUI.java
@@ -4,13 +4,14 @@ import com.envyful.api.concurrency.UtilConcurrency;
 import com.envyful.api.forge.chat.UtilChatColour;
 import com.envyful.api.forge.concurrency.UtilForgeConcurrency;
 import com.envyful.api.forge.player.ForgeEnvyPlayer;
-import com.envyful.api.reforged.dialogue.DialogueInputRegistry;
 import com.envyful.api.time.UtilTime;
 import com.envyful.api.time.UtilTimeFormat;
 import com.envyful.gts.forge.EnvyGTSForge;
 import com.envyful.gts.forge.impl.trade.ForgeTrade;
 import com.envyful.gts.forge.impl.trade.type.PokemonTrade;
 import com.envyful.gts.forge.player.GTSAttribute;
+import com.pixelmonmod.pixelmon.api.dialogue.DialogueButton;
+import com.pixelmonmod.pixelmon.api.dialogue.DialogueFactory;
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import com.pixelmonmod.pixelmon.api.storage.PCBox;
 import com.pixelmonmod.pixelmon.api.storage.PlayerPartyStorage;
@@ -25,9 +26,9 @@ public class EditDurationUI {
         GTSAttribute attribute = player.getAttributeNow(GTSAttribute.class);
         Pokemon pokemon = SelectPriceUI.getPokemon(player, page, position);
 
-        UtilForgeConcurrency.runLater(() -> DialogueInputRegistry.builder()
+        UtilForgeConcurrency.runLater(() -> DialogueFactory.builder()
                 .title(UtilChatColour.colour(EnvyGTSForge.getLocale().getDurationInputDialogueTitle()))
-                .text(UtilChatColour.colour((!error ?
+                .description(UtilChatColour.colour((!error ?
                         EnvyGTSForge.getLocale().getDurationInputDialogueText() :
                         EnvyGTSForge.getLocale().getDurationInputDialogueErrorText())
                         .replace("%min_price%", String.format(EnvyGTSForge.getLocale().getMoneyFormat(), attribute.getCurrentPrice()))
@@ -38,57 +39,60 @@ public class EditDurationUI {
                                 TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMinTradeDuration())
                         ))
                         .replace("%pokemon%", pokemon.getDisplayName())))
-                .defaultInputValue(TimeUnit.SECONDS.toMinutes(EnvyGTSForge.getConfig().getMinTradeDuration()) + "m")
+                .defaultText(TimeUnit.SECONDS.toMinutes(EnvyGTSForge.getConfig().getMinTradeDuration()) + "m")
                 .closeOnEscape()
-                .closeHandler(closedScreen -> {
+                .onClose(closedScreen -> {
                     if (page == -1) {
                         SelectPartyPokemonUI.openUI(player);
                     } else {
                         SelectPCPokemonUI.openUI(player, page);
                     }
                 })
-                .submitHandler(submitted -> {
-                    UtilConcurrency.runAsync(() -> {
-                        long inputtedValue = UtilTime.attemptParseTime(submitted.getInput()).orElse(-1L);
+                .buttons(DialogueButton.builder()
+                        .text("Submit")
+                        .onClick(submitted -> {
+                            UtilConcurrency.runAsync(() -> {
+                                long inputtedValue = UtilTime.attemptParseTime(submitted.getInput()).orElse(-1L);
 
-                        if (inputtedValue < TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMinTradeDuration()) || inputtedValue < 0) {
-                            openUI(player, page, position, true);
-                            return;
-                        }
+                                if (inputtedValue < TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMinTradeDuration()) || inputtedValue < 0) {
+                                    openUI(player, page, position, true);
+                                    return;
+                                }
 
-                        if (inputtedValue > TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMaxTradeDurationSeconds())) {
-                            openUI(player, page, position, true);
-                            return;
-                        }
+                                if (inputtedValue > TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMaxTradeDurationSeconds())) {
+                                    openUI(player, page, position, true);
+                                    return;
+                                }
 
-                        Pokemon pixelmon;
+                                Pokemon pixelmon;
 
-                        if (page == -1) {
-                            PlayerPartyStorage party = StorageProxy.getPartyNow(player.getParent());
-                            pixelmon = party.get(position);
-                            party.set(position, null);
-                        } else {
-                            PCBox box = StorageProxy.getPCForPlayerNow(player.getParent()).getBox(page);
-                            pixelmon = box.get(position);
-                            box.set(position, null);
-                        }
+                                if (page == -1) {
+                                    PlayerPartyStorage party = StorageProxy.getPartyNow(player.getParent());
+                                    pixelmon = party.get(position);
+                                    party.set(position, null);
+                                } else {
+                                    PCBox box = StorageProxy.getPCForPlayerNow(player.getParent()).getBox(page);
+                                    pixelmon = box.get(position);
+                                    box.set(position, null);
+                                }
 
-                        EnvyGTSForge.getTradeManager()
-                                .addTrade(player, ((PokemonTrade.Builder) ForgeTrade.builder()
-                                        .cost(attribute.getCurrentPrice())
-                                        .expiry(System.currentTimeMillis() + inputtedValue)
-                                        .owner(player)
-                                        .originalOwnerName(player.getName())
-                                        .content("p"))
-                                        .contents(pixelmon)
-                                        .build());
-                        attribute.setCurrentDuration(0);
-                        attribute.setCurrentMinPrice(0);
-                        attribute.setCurrentPrice(0);
-                        attribute.setSelectedSlot(-1);
-                    });
-                })
-                .open(player.getParent()), 5);
+                                EnvyGTSForge.getTradeManager()
+                                        .addTrade(player, ((PokemonTrade.Builder) ForgeTrade.builder()
+                                                .cost(attribute.getCurrentPrice())
+                                                .expiry(System.currentTimeMillis() + inputtedValue)
+                                                .owner(player)
+                                                .originalOwnerName(player.getName())
+                                                .content("p"))
+                                                .contents(pixelmon)
+                                                .build());
+                                attribute.setCurrentDuration(0);
+                                attribute.setCurrentMinPrice(0);
+                                attribute.setCurrentPrice(0);
+                                attribute.setSelectedSlot(-1);
+                            });
+                        })
+                        .build())
+                .sendTo(player.getParent()), 5);
     }
 
 }
--- forge20/src/main/java/com/envyful/gts/forge/ui/EditItemDurationUI.java
+++ forge20/src/main/java/com/envyful/gts/forge/ui/EditItemDurationUI.java
@@ -4,11 +4,12 @@ import com.envyful.api.forge.chat.UtilChatColour;
 import com.envyful.api.forge.concurrency.UtilForgeConcurrency;
 import com.envyful.api.forge.player.ForgeEnvyPlayer;
 import com.envyful.api.forge.player.util.UtilPlayer;
-import com.envyful.api.reforged.dialogue.DialogueInputRegistry;
 import com.envyful.api.time.UtilTime;
 import com.envyful.api.time.UtilTimeFormat;
 import com.envyful.gts.forge.EnvyGTSForge;
 import com.envyful.gts.forge.player.GTSAttribute;
+import com.pixelmonmod.pixelmon.api.dialogue.DialogueButton;
+import com.pixelmonmod.pixelmon.api.dialogue.DialogueFactory;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
 
@@ -21,9 +22,9 @@ public class EditItemDurationUI {
         GTSAttribute attribute = player.getAttributeNow(GTSAttribute.class);
         ItemStack itemInHand = player.getParent().getItemInHand(InteractionHand.MAIN_HAND);
 
-        UtilForgeConcurrency.runLater(() -> DialogueInputRegistry.builder()
+        UtilForgeConcurrency.runLater(() -> DialogueFactory.builder()
                 .title(UtilChatColour.colour(EnvyGTSForge.getLocale().getDurationInputDialogueTitle()))
-                .text(UtilChatColour.colour((!error ?
+                .description(UtilChatColour.colour((!error ?
                         EnvyGTSForge.getLocale().getDurationInputDialogueText() :
                         EnvyGTSForge.getLocale().getDurationInputDialogueErrorText())
                         .replace("%min_price%", String.format(EnvyGTSForge.getLocale().getMoneyFormat(), attribute.getCurrentPrice()))
@@ -34,25 +35,29 @@ public class EditItemDurationUI {
                                 TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMinTradeDuration())
                         ))
                         .replace("%item%", itemInHand.getHoverName().getString())))
-                .defaultInputValue(TimeUnit.SECONDS.toMinutes(EnvyGTSForge.getConfig().getMinTradeDuration()) + "m")
+                .showInput()
+                .defaultText(TimeUnit.SECONDS.toMinutes(EnvyGTSForge.getConfig().getMinTradeDuration()) + "m")
                 .closeOnEscape()
-                .closeHandler(closedScreen -> SellHandOrParty.open(player))
-                .submitHandler(submitted -> {
-                    long inputtedValue = UtilTime.attemptParseTime(submitted.getInput()).orElse(-1L);
+                .onClose(closedScreen -> SellHandOrParty.open(player))
+                .buttons(DialogueButton.builder()
+                        .text("Submit")
+                        .onClick(submitted -> {
+                            long inputtedValue = UtilTime.attemptParseTime(submitted.getInput()).orElse(-1L);
 
-                    if (inputtedValue < TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMinTradeDuration()) || inputtedValue < 0) {
-                        openUI(player, time,true);
-                        return;
-                    }
+                            if (inputtedValue < TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMinTradeDuration()) || inputtedValue < 0) {
+                                openUI(player, time, true);
+                                return;
+                            }
 
-                    if (inputtedValue > TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMaxTradeDurationSeconds())) {
-                        openUI(player, time,true);
-                        return;
-                    }
+                            if (inputtedValue > TimeUnit.SECONDS.toMillis(EnvyGTSForge.getConfig().getMaxTradeDurationSeconds())) {
+                                openUI(player, time, true);
+                                return;
+                            }
 
-                    UtilPlayer.runCommand(player.getParent(), "gts sell " + itemInHand.getCount() + " " + time + " " + submitted.getInput());
-                })
-                .open(player.getParent()), 5);
+                            UtilPlayer.runCommand(player.getParent(), "gts sell " + itemInHand.getCount() + " " + time + " " + submitted.getInput());
+                        })
+                        .build())
+                .sendTo(player.getParent()), 5);
     }
 
 }
