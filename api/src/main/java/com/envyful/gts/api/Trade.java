67305799479edc0918a41c788b4349c54e6621de
Daniel Norris <dnorris443@gmail.com>
2024-11-29 14:17:04 +0000

    chore(gradle): update API

--- api/src/main/java/com/envyful/gts/api/Trade.java
+++ api/src/main/java/com/envyful/gts/api/Trade.java
@@ -167,7 +167,7 @@ public interface Trade extends Placeholder {
 
     @Override
     default @NonNull ParseResult replace(@NonNull ParseResult parseResult) {
-        for (Placeholder placeholder : this.placeholders()) {
+        for (var placeholder : this.placeholders()) {
             parseResult = placeholder.replace(parseResult);
         }
 
